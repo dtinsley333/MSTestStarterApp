04d50032588232b2f0da51a10a81bc172f31df3b
dtinsley333 <neecetinsley@icloud.com>
2016-11-08 13:34:39 -0600

    initial check in

---
+++ BangazonPrimeTests/BangazonPrimeUserTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BangazonPrimeTests
+{
+    [TestClass]
+    public class BangazonPrime
+    {
+        [TestMethod]
+        public void UserIsBangazonPrimeUser()
+        {
+            User bangazonUser = new User(DateTime.Now.AddMonths(-1), null, 123);//no end date means they have Bangazon prime
+            bool userIsPrime = bangazonUser.IsPrimeUser;
+            Assert.IsTrue(userIsPrime);
+        }
+
+        [TestMethod]
+        public void UserIsNotBangazonPrimeUser()
+        {
+            User bangazonUser = new User(DateTime.Now.AddMonths(-11), DateTime.Now.AddMonths(-1), 344); //End Date in the past means no Bangazon prime
+            bool userIsPrime = bangazonUser.IsPrimeUser;
+            Assert.IsFalse(userIsPrime);
+        }
+    }
+}
---
+++ BangazonPrimeTests/BangazonProductTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BangazonPrimeTests
+{
+    [TestClass]
+    class BangazonProductTests
+    {
+        [TestMethod]
+        public void ProductIsBangazonPrimeEligible()
+        {
+            Product bangazonProduct = new Product(productId);
+            bool isPrimeProduct = bangazonProduct.IsPrimeProduct;
+            Assert.IsTrue(isPrimeProduct);
+        }
+
+        [TestMethod]
+        public void ProductIsNotBangazonPrimeEligible()
+        {
+            Product bangazonProduct = new Product(productId);
+            bool isPrimeProduct = bangazonProduct.IsPrimeProduct;
+            Assert.IsFalse(isPrimeProduct);
+        }
+
+    }
+}
